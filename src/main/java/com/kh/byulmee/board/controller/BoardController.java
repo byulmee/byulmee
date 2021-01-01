c4efcc85213f955b8e1410861c4ad39f92b39eaa
SangYong <bsy2274@naver.com>
2021-01-01 19:42:12 +0900

    상용
    고객센터 QnA리스트

---
+++ src/main/java/com/kh/byulmee/board/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.kh.byulmee.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping("QnAView.bo")
+	public String QnAListForm() {
+		return "QnAList";
+	}
+}
