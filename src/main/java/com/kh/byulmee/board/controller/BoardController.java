df61d5490eafd58c810f68284dd7ee47353db50e
SangYong <bsy2274@naver.com>
2021-01-05 16:33:45 +0900

    20210105 백상용
    고객센터 브릿지 페이지 생성
    
    Q&A게시판 연결
    
    공지사항 게시판 생성 & 연결
    
    공지사항 게시글 작성페이지 생성 & 연결

--- src/main/java/com/kh/byulmee/board/controller/BoardController.java
+++ src/main/java/com/kh/byulmee/board/controller/BoardController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 	
+	@RequestMapping("ccView.bo")
+	public String CustomerCenterForm() {
+		return "centerBridge";
+	}
+	
 	@RequestMapping("QnAView.bo")
 	public String QnAListForm() {
 		return "QnAList";
@@ -14,5 +19,15 @@ public class BoardController {
 	@RequestMapping("QnAInsertView.bo")
 	public String QnAInsertForm() {
 		return "QnAInsert";
+	}
+	
+	@RequestMapping("noticeListView.bo")
+	public String NoticeListForm() {
+		return "noticeList";
+	}
+	
+	@RequestMapping("noticeInsertView.bo")
+	public String NoticeInsertForm() {
+		return "noticeInsert";
 	}
 }
