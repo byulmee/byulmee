ec108039d79dcfa66ce7e39f82c7d7c5b866dea3
duoh <plan0027@gmail.com>
2021-01-07 16:56:28 +0900

    210107 다혜 lombok 추가 및 VO 클래스 수정

--- src/main/java/com/kh/byulmee/member/model/vo/Address.java
+++ src/main/java/com/kh/byulmee/member/model/vo/Address.java
@@ -1,5 +1,16 @@
 package com.kh.byulmee.member.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 	private int addNo;
 	private String memId;
@@ -10,99 +21,4 @@ public class Address {
 	private String addDetailaddr;
 	private String addPhone;
 	private String addChoice;
-	
-	public Address() {}
-			
-	public Address(int addNo, String memId, String addPlace, String addName, int addPostcode, String addBasicaddr,
-			String addDetailaddr, String addPhone, String addChoice) {
-		super();
-		this.addNo = addNo;
-		this.memId = memId;
-		this.addPlace = addPlace;
-		this.addName = addName;
-		this.addPostcode = addPostcode;
-		this.addBasicaddr = addBasicaddr;
-		this.addDetailaddr = addDetailaddr;
-		this.addPhone = addPhone;
-		this.addChoice = addChoice;
-	}
-	
-	public int getAddNo() {
-		return addNo;
-	}
-
-	public void setAddNo(int addNo) {
-		this.addNo = addNo;
-	}
-
-	public String getMemId() {
-		return memId;
-	}
-
-	public void setMemId(String memId) {
-		this.memId = memId;
-	}
-
-	public String getAddPlace() {
-		return addPlace;
-	}
-
-	public void setAddPlace(String addPlace) {
-		this.addPlace = addPlace;
-	}
-
-	public String getAddName() {
-		return addName;
-	}
-
-	public void setAddName(String addName) {
-		this.addName = addName;
-	}
-
-	public int getAddPostcode() {
-		return addPostcode;
-	}
-
-	public void setAddPostcode(int addPostcode) {
-		this.addPostcode = addPostcode;
-	}
-
-	public String getAddBasicaddr() {
-		return addBasicaddr;
-	}
-
-	public void setAddBasicaddr(String addBasicaddr) {
-		this.addBasicaddr = addBasicaddr;
-	}
-
-	public String getAddDetailaddr() {
-		return addDetailaddr;
-	}
-
-	public void setAddDetailaddr(String addDetailaddr) {
-		this.addDetailaddr = addDetailaddr;
-	}
-
-	public String getAddPhone() {
-		return addPhone;
-	}
-
-	public void setAddPhone(String addPhone) {
-		this.addPhone = addPhone;
-	}
-
-	public String getAddChoice() {
-		return addChoice;
-	}
-
-	public void setAddChoice(String addChoice) {
-		this.addChoice = addChoice;
-	}
-
-	@Override
-	public String toString() {
-		return "Address [addNo=" + addNo + ", memId=" + memId + ", addPlace=" + addPlace + ", addName=" + addName
-				+ ", addPostcode=" + addPostcode + ", addBasicaddr=" + addBasicaddr + ", addDetailaddr=" + addDetailaddr
-				+ ", addPhone=" + addPhone + ", addChoice=" + addChoice + "]";
-	}
 }
